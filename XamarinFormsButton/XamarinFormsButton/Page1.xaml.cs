791c8e6b5b83ee384ad30d98dc40d6a43c5a1aa0
kobake <kobake@users.sourceforge.net>
2016-09-03 15:39:23 +0900

    Button events

--- XamarinFormsButton/XamarinFormsButton/Page1.xaml.cs
+++ XamarinFormsButton/XamarinFormsButton/Page1.xaml.cs
@@ -10,9 +10,27 @@ namespace XamarinFormsButton
 {
 	public partial class Page1 : ContentPage
 	{
+		private int m_count;
+
 		public Page1()
 		{
 			InitializeComponent();
 		}
+
+		public void Button_Clicked(object sender, EventArgs e)
+		{
+			Button button = sender as Button;
+			switch (button.Text)
+			{
+				case "Plus":
+					m_count++;
+					break;
+				case "Minus":
+					m_count--;
+					break;
+			}
+			MainLabel.Text = "Count: " + m_count;
+		}
 	}
+
 }
